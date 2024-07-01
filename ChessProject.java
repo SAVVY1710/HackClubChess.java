ba32c28dc4f5192e900c1eabb5221659ee427e5c
SAVVY1710 <163214365+SAVVY1710@users.noreply.github.com>
2024-06-30 22:17:37 -0700

    Added White pieces

--- ChessProject.java
+++ ChessProject.java
@@ -60,8 +60,25 @@ class HGPanel extends JPanel {
         setBackground(Color.green);
         y = 0;
         x = 7;
+        whiterook1 = new ImageIcon("White_Rook.png").getImage();
+        whiterook2 = new ImageIcon("White_Rook.png").getImage();
+        whitehorse1 = new ImageIcon("White_Horse.png").getImage();
+        whitehorse2 = new ImageIcon("White_Horse.png").getImage();
+        whitebishop1 = new ImageIcon("White_Bishop.png").getImage();
+        whitebishop2 = new ImageIcon("White_Bishop.png").getImage();
+        whiteking = new ImageIcon("White_King.png").getImage();
+        whitequeen = new ImageIcon("White_Queen.png").getImage();
+        whitepawn1 = new ImageIcon("White_Pawn.png").getImage();
+        whitepawn2 = new ImageIcon("White_Pawn.png").getImage();
+        whitepawn3 = new ImageIcon("White_Pawn.png").getImage();
+        whitepawn4 = new ImageIcon("White_Pawn.png").getImage();
+        whitepawn6 = new ImageIcon("White_Pawn.png").getImage();
+        whitepawn7 = new ImageIcon("White_Pawn.png").getImage();
+        whitepawn8 = new ImageIcon("White_Pawn.png").getImage();
+        whitepawn5 = new ImageIcon("White_Pawn.png").getImage();
         squares = new Rectangle[8][8];
         squarepoint = new Point[8][8];
+        wpawnrect = new Rectangle[8];
         for (int i = 0; i < 8; i++) {
             for (int n = 0; n < 8; n++) {
                 squarepoint[i][n] = new Point(x, y);
@@ -77,6 +94,22 @@ class HGPanel extends JPanel {
                 squares[i][n] = new Rectangle((int) squarepoint[i][n].getX(), (int) squarepoint[i][n].getY(), 90, 90);
             }
         }
+        wrookrect1 = new Rectangle((int) squarepoint[0][7].getX(), (int) squarepoint[0][7].getY(), 90, 90);
+        wrookrect2 = new Rectangle((int) squarepoint[7][7].getX(), (int) squarepoint[7][7].getY(), 90, 90);
+        whorserect1 = new Rectangle((int) squarepoint[1][7].getX(), (int) squarepoint[1][7].getY(), 90, 90);
+        whorserect2 = new Rectangle((int) squarepoint[6][7].getX(), (int) squarepoint[6][7].getY(), 90, 90);
+        wbishoprect1 = new Rectangle((int) squarepoint[2][7].getX(), (int) squarepoint[2][7].getY(), 90, 90);
+        wbishoprect2 = new Rectangle((int) squarepoint[5][7].getX(), (int) squarepoint[5][7].getY(), 90, 90);
+        wkingrect = new Rectangle((int) squarepoint[3][7].getX(), (int) squarepoint[3][7].getY(), 90, 90);
+        wqueenrect = new Rectangle((int) squarepoint[4][7].getX(), (int) squarepoint[4][7].getY(), 90, 90);
+        wpawnrect[0] = new Rectangle((int) squarepoint[0][6].getX(), (int) squarepoint[0][6].getY(), 90, 90);
+        wpawnrect[1] = new Rectangle((int) squarepoint[1][6].getX(), (int) squarepoint[1][6].getY(), 90, 90);
+        wpawnrect[2] = new Rectangle((int) squarepoint[2][6].getX(), (int) squarepoint[2][6].getY(), 90, 90);
+        wpawnrect[3] = new Rectangle((int) squarepoint[3][6].getX(), (int) squarepoint[3][6].getY(), 90, 90);
+        wpawnrect[4] = new Rectangle((int) squarepoint[4][6].getX(), (int) squarepoint[4][6].getY(), 90, 90);
+        wpawnrect[5] = new Rectangle((int) squarepoint[5][6].getX(), (int) squarepoint[5][6].getY(), 90, 90);
+        wpawnrect[6] = new Rectangle((int) squarepoint[6][6].getX(), (int) squarepoint[6][6].getY(), 90, 90);
+        wpawnrect[7] = new Rectangle((int) squarepoint[7][6].getX(), (int) squarepoint[7][6].getY(), 90, 90);
     }
     public void paintComponent(Graphics g) {
         for (int i = 0; i < 8; i++) {
@@ -96,6 +129,22 @@ class HGPanel extends JPanel {
             }
         }
 
-    }
+        g.drawImage(whiterook1, (int)wrookrect1.getX(), (int)wrookrect1.getY(), 90, 90, null);
+        g.drawImage(whiterook2, (int)wrookrect2.getX(), (int)wrookrect2.getY(), 90, 90, null);
+        g.drawImage(whitehorse1, (int) whorserect1.getX(), (int) whorserect1.getY(), 90, 90, null);
+        g.drawImage(whitehorse2, (int) whorserect2.getX(), (int) whorserect2.getY(), 90, 90, null);
+        g.drawImage(whitebishop1, (int) wbishoprect1.getX(), (int) wbishoprect1.getY(), 90, 90, null);
+        g.drawImage(whitebishop2, (int) wbishoprect2.getX(), (int) wbishoprect2.getY(), 90, 90, null);
+        g.drawImage(whiteking, (int) wkingrect.getX(), (int) wkingrect.getY(), 90, 90, null);
+        g.drawImage(whitequeen, (int) wqueenrect.getX(), (int) wqueenrect.getY(), 90, 90, null);
+        g.drawImage(whitepawn1, (int) wpawnrect[0].getX(), (int) wpawnrect[0].getY(), 90, 90, null);
+        g.drawImage(whitepawn2, (int) wpawnrect[1].getX(), (int) wpawnrect[1].getY(), 90, 90, null);
+        g.drawImage(whitepawn3, (int) wpawnrect[2].getX(), (int) wpawnrect[2].getY(), 90, 90, null);
+        g.drawImage(whitepawn4, (int) wpawnrect[3].getX(), (int) wpawnrect[3].getY(), 90, 90, null);
+        g.drawImage(whitepawn5, (int) wpawnrect[4].getX(), (int) wpawnrect[4].getY(), 90, 90, null);
+        g.drawImage(whitepawn6, (int) wpawnrect[5].getX(), (int) wpawnrect[5].getY(), 90, 90, null);
+        g.drawImage(whitepawn7, (int) wpawnrect[6].getX(), (int) wpawnrect[6].getY(), 90, 90, null);
+        g.drawImage(whitepawn8, (int) wpawnrect[7].getX(), (int) wpawnrect[7].getY(), 90, 90, null);
 
+    }
 }
